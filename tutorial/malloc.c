5941b6663105a610aecb4a25073ea94938045d7c
camillexue <xue.camille@gmail.com>
2019-03-10 15:50:31 -0400

    found a malloc test online and trying to get it working on tutorial implementation

--- tutorial/malloc.c
+++ tutorial/malloc.c
@@ -10,18 +10,18 @@ void *global_base = NULL; // head of linked list
 struct block_meta {
   size_t size;
   struct block_meta *next;
-  int free;
+  int my_free;
 };
 
-/* find_free_block: iterates through linked list looking
-  for free block of correct size
+/* find_my_free_block: iterates through linked list looking
+  for my_free block of correct size
 
-  returns: free block
+  returns: my_free block
 */
-struct block_meta *find_free_block(struct block_meta **last, size_t size) {
+struct block_meta *find_my_free_block(struct block_meta **last, size_t size) {
   struct block_meta *current = global_base;
 
-  while(current && !(current->free && current->size >= size)) {
+  while(current && !(current->my_free && current->size >= size)) {
     *last = current;
     current = current->next;
   }
@@ -34,7 +34,7 @@ struct block_meta *find_free_block(struct block_meta **last, size_t size) {
 
   returns: newly allocated block
 */
-struct block_meta *request_space(sturct block_meta last, size_t size) {
+struct block_meta *request_space(struct block_meta *last, size_t size) {
   struct block_meta *block;
   block = sbrk(0); // points to current top of heap
   void *request = sbrk(size + META_SIZE);
@@ -44,43 +44,43 @@ struct block_meta *request_space(sturct block_meta last, size_t size) {
   }
 
   if(last) { // add to linked list if not first request
-    last->next = block
+    last->next = block;
   }
 
   block->size = size;
   block->next = NULL;
-  block->free = 0;
+  block->my_free = 0;
   return block;
 }
 
-/* malloc: allocates space on heap using sbrk
+/* my_malloc: allocates space on heap using sbrk
 
   size_t: number of bytes to allocate
 
   returns: pointer to allocated space
 */
-void *malloc(size_t size) {
-  struct block_meta block;
+void *my_malloc(size_t size) {
+  struct block_meta *block;
   if(size <= 0) {
     return NULL;
   }
 
   if(!global_base){ // if first call
     block = request_space(NULL, size);
-    if(!block){ //request space failed?
+    if(!block){ //request space faileEd?
       return NULL;
     }
     global_base = block;
   } else {
     struct block_meta *last = global_base;
-    block = find_free_block(&last, size);
-    if(!block) { // failed to find free block
+    block = find_my_free_block(&last, size);
+    if(!block) { // failed to find my_free block
       block = request_space(NULL, size);
       if(!block){
         return NULL;
       }
-    } else { // found free block
-      block->free = 0;
+    } else { // found my_free block
+      block->my_free = 0;
     }
   }
   return(block+1);
@@ -92,12 +92,40 @@ struct block_meta *get_block_ptr(void *ptr) {
   return (struct block_meta*)ptr-1;
 }
 
-// free: frees block at given pointer
-void free(void*ptr) {
+// my_free: my_frees block at given pointer
+void my_free(void*ptr) {
   if(!ptr) {
     return;
   }
   struct block_meta* block_ptr = get_block_ptr(ptr);
-  assert(block_ptr->free == 0);
-  block_ptr->free = 1;
+  assert(block_ptr->my_free == 0);
+  block_ptr->my_free = 1;
+}
+
+void *my_realloc(void *ptr, size_t size) {
+  if(!ptr) {
+    return my_malloc(size);
+  }
+
+  struct block_meta* block_ptr = get_block_ptr(ptr);
+  if(block_ptr->size >=size) {
+    // we have enough space
+    return ptr;
+  }
+
+  void *new_ptr;
+  new_ptr = (my_malloc(size));
+  if(!new_ptr) {
+    return NULL;
+  }
+  memcpy(new_ptr, ptr, block_ptr->size);
+  my_free(ptr);
+  return new_ptr;
+}
+
+void *my_calloc(size_t nelem, size_t elsize) {
+  size_t size = nelem*elsize;
+  void *ptr = my_malloc(size);
+  memset(ptr, 0,size);
+  return ptr;
 }
